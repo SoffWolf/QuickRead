bba20b1262dc44f85258f83ea4fb5ddfdd3a34b5
SoffWolf <tungnguyen21012000@gmail.com>
2021-12-03 15:31:40 +0700

    Add reward training file

--- reward_model.py
+++ reward_model.py
@@ -6,7 +6,7 @@ class RewardModel(nn.Module):
         super(RewardModel, self).__init__()
         self.supervised_baseline= supervised_baseline
         self.reward_head = nn.Sequential(
-                nn.Linear(..., ...))
+                nn.Linear(..., ...)) #check the final layer of Pegasus and decide how to write the " randomly initialized linear head that outputs a scalar value" here
 
     def forward(self, inputs):
         x = self.model(inputs)
---
+++ reward_model_training.py
@@ -0,0 +1,5 @@
+import torch
+import torch.nn as nn
+import torch.functional as F
+
+####write the training loop here
