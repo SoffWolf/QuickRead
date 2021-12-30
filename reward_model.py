07c753d67da5b5800354998b67b23f5390a9f9f3
SoffWolf <tungnguyen21012000@gmail.com>
2021-12-30 21:50:06 +0200

    Finish the first version of training loop for reward model

--- reward_model.py
+++ reward_model.py
@@ -1,17 +1,16 @@
 import torch
 import torch.nn as nn
 import numpy as np
+import torch.nn.functional as F
 
 
 class RewardModel(nn.Module):
-    def __init__(self, supervised_baseline, d_model=64, init_scales=0.0):
+    def __init__(self, supervised_baseline, d_model=64, init_scales=1.0):
         super(RewardModel, self).__init__()
-
-        self.post_input = supervised_baseline
-        self.summary_input = supervised_baseline
-        self.final = nn.Sequential(nn.Linear(d_model, d_model))
-
+        self.d_model = d_model
+        self.supervised_baseline = supervised_baseline
         # Add a randomly initialized linear head that outputs a scalar value
+
         init_std = init_scales / np.sqrt(d_model + 1)  #.get(name, 1.0)
         head = nn.Linear(d_model, 1)
         nn.init.normal_(head.weight, std=init_std)
@@ -19,12 +18,12 @@ class RewardModel(nn.Module):
         self.head = head
 
     def forward(self, post_tokens, summary_tokens):
-        x = self.post_input.generate(input_ids=post_tokens)
-        y = self.summary_input.generate(input_ids=summary_tokens)
-        z = torch.concat((x, y), axis=1)
-        z = self.final(z.type(torch.FloatTensor))
+        x = torch.concat((post_tokens, summary_tokens), axis=1) 
+        x = self.supervised_baseline.generate(input_ids=x)
+        x = F.pad(input=x, pad=(1, self.d_model - x.shape[1] - 1), mode='constant', value=0)
+        print(x.shape)
         # go through custom layer
-        reward = self.head(z)
+        reward = self.head(x.type(torch.FloatTensor))
         return reward
 
 
--- reward_model_training_alt.py
+++ reward_model_training_alt.py
@@ -55,8 +55,8 @@ for num in numbers:
 
 
 df = pd.DataFrame(data, columns=columns)
-tokenizer = AutoTokenizer.from_pretrained("SophieTr/results")
-supervised_baseline = AutoModelForSeq2SeqLM.from_pretrained("SophieTr/results")
+tokenizer = AutoTokenizer.from_pretrained("sshleifer/distill-pegasus-xsum-16-4")
+supervised_baseline = AutoModelForSeq2SeqLM.from_pretrained("sshleifer/distill-pegasus-xsum-16-4")
 
 
 
@@ -92,94 +92,122 @@ class Dataset(torch.utils.data.Dataset):
 
 
 np.random.seed(112)
-df_train, df_val, df_test = np.split(df.sample(frac=1, random_state=42), [int(.8*len(df)), int(.9*len(df))])
+df_train, df_val, df_test = np.split(df.sample(frac=1, random_state=42), [int(.9*len(df)), int(.95*len(df))])
 
 print(len(df_train),len(df_val), len(df_test))
 
 model = RewardModel(supervised_baseline)
-post = """Title said just about all of it. I’m 28, very athletic (bike/ surf/ snowboard) and I have always wanted to do gymnastics.
-
-I like to do flips and spins off bridges and on my snowboard, and it seems to me gymnastics would be a great way to do those movements I like, in a controlled environment. The end goal of this is that it would be fun, and make me better at these movements in real life.
-
-But is it too late for me? Should 28 year old guys such as myself be content with just watching those parkour guys on youtube? Or can I learn the ways of the gymnastic jedi? BTW, I live in San Jose CA."""
+post = """We use a 774M parameter version of the GPT-2 language
+model in Radford et al. (2019) trained on their WebText
+dataset and their 50,257 token invertible byte pair encoding
+to preserve capitalization and punctuation (Sennrich et al.,
+2015). The model is a Transformer with 36 layers, 20 heads,
+and embedding size 1280 (Vaswani et al., 2017).
+For stylistic continuation tasks we perform supervised finetuning of the language model to the BookCorpus dataset
+of Zhu et al. (2015) prior to RL fine-tuning; we train from
+scratch on WebText, supervised fine-tune on BookCorpus,
+then RL fine-tune to our final task. To improve sample
+quality, we use a temperature of T < 1 for all experiments;
+we modify the initial language model by dividing logits by
+T, so that future sampling and RL with T = 1 corresponds
+to a lower temperature for the unmodified pretrained model."""
 summary = "I want to do gymnastics, but I’m 28 yrs old. Is it too late for me to be a gymnaste?!"
 post_id = tokenizer(post, return_tensors="pt").input_ids
 sum_id = tokenizer(summary, return_tensors="pt").input_ids
 print(model(post_id, sum_id))
 
 # training loop
-# def train(model, train_data, val_data, learning_rate, epochs):
-# 
-#     def criterion(x):
-#         return np.log(1/(1 + np.exp(-x)))
-# 
-#     train, val = Dataset(train_data), Dataset(val_data)
-# 
-#     train_dataloader = torch.utils.data.DataLoader(train, batch_size=2, shuffle=True)
-#     val_dataloader = torch.utils.data.DataLoader(val, batch_size=2)
-# 
-#     use_cuda = torch.cuda.is_available()
-#     device = torch.device("cuda" if use_cuda else "cpu")
-# 
-#     # criterion = nn.CrossEntropyLoss()
-#     optimizer = Adam(model.parameters(), lr= learning_rate)
-# 
-#     if use_cuda:
-#         model = model.cuda()
-# 
-#     for epoch_num in range(epochs):
-#         total_acc_train = 0
-#         total_loss_train = 0
-# 
-#         for post, split, sum1, sum2, label in tqdm(train_dataloader):
-#             # mask_post = post['attention_mask'].to(device)
-#             # mask_sum1 = sum1['attention_mask'].to(device)
-#             # mask_sum2 = sum2['attention_mask'].to(device)
-#             post_id = post['input_ids'].squeeze(1).to(device)
-#             sum1_id = sum1['input_ids'].squeeze(1).to(device)
-#             sum2_id = sum2['input_ids'].squeeze(1).to(device)
-#             label = label.to(device)
-# 
-#             output = model(input_id)
-#             
-#             batch_loss = criterion(output, train_label)
-#             total_loss_train += batch_loss.item()
-#             
-#             acc = (output.argmax(dim=1) == train_label).sum().item()
-#             total_acc_train += acc
-# 
-#             model.zero_grad()
-#             batch_loss.backward()
-#             optimizer.step()
-#         
-#         total_acc_val = 0
-#         total_loss_val = 0
-# 
-#         with torch.no_grad():
-# 
-#             for val_input, val_label in val_dataloader:
-# 
-#                 val_label = val_label.to(device)
-#                 mask = val_input['attention_mask'].to(device)
-#                 input_id = val_input['input_ids'].squeeze(1).to(device)
-# 
-#                 output = model(input_id, mask)
-# 
-#                 batch_loss = criterion(output, val_label)
-#                 total_loss_val += batch_loss.item()
-#                 
-#                 acc = (output.argmax(dim=1) == val_label).sum().item()
-#                 total_acc_val += acc
-#         
-#         print(
-#             f'Epochs: {epoch_num + 1} | Train Loss: {total_loss_train / len(train_data): .3f} \
-#             | Train Accuracy: {total_acc_train / len(train_data): .3f} \
-#             | Val Loss: {total_loss_val / len(val_data): .3f} \
-#             | Val Accuracy: {total_acc_val / len(val_data): .3f}')
-#                   
-# EPOCHS = 5
-# LR = 1e-6
-# train(model, df_train, df_val, LR, EPOCHS)
+def train(model, train_data, val_data, learning_rate, epochs):
+
+    def criterion(x):
+        return np.log(1/(1 + np.exp(-x)))
+
+    train, val = Dataset(train_data), Dataset(val_data)
+
+    train_dataloader = torch.utils.data.DataLoader(train, batch_size=8, shuffle=True)
+    val_dataloader = torch.utils.data.DataLoader(val, batch_size=8)
+
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    # criterion = nn.CrossEntropyLoss()
+    optimizer = Adam(model.parameters(), lr= learning_rate)
+
+    if use_cuda:
+        model = model.cuda()
+
+    for epoch_num in range(epochs):
+        total_acc_train = 0
+        total_loss_train = 0
+
+        for post, split, sum1, sum2, label in tqdm(train_dataloader):
+            # mask_post = post['attention_mask'].to(device)
+            # mask_sum1 = sum1['attention_mask'].to(device)
+            # mask_sum2 = sum2['attention_mask'].to(device)
+            post_id = tokenizer(post)['input_ids'].squeeze(1).to(device)
+            sum1_id = tokenizer(sum1)['input_ids'].squeeze(1).to(device)
+            sum2_id = tokenizer(sum2)['input_ids'].squeeze(1).to(device)
+                    
+            label = label.to(device)
+            predicted_reward_1 = None
+            predicted_reward_2 = None
+
+            if label == 0:
+                predicted_reward_1 = model(post_id, sum1_id)
+                predicted_reward_2 = model(post_id, sum2_id)
+            else:
+                predicted_reward_2 = model(post_id, sum1_id)
+                predicted_reward_1 = model(post_id, sum2_id)
+        
+            
+            batch_loss = criterion(predicted_reward_1 - predicted_reward_2)
+            total_loss_train += batch_loss.item()
+            
+            # acc = (output.argmax(dim=1) == train_label).sum().item()
+            # total_acc_train += acc
+
+            model.zero_grad()
+            batch_loss.backward()
+            optimizer.step()
+        
+        total_acc_val = 0
+        total_loss_val = 0
+
+        with torch.no_grad():
+
+            for post, split, sum1, sum2, label in tqdm(val_dataloader):
+                post_id = tokenizer(post)['input_ids'].squeeze(1).to(device)
+                sum1_id = tokenizer(sum1)['input_ids'].squeeze(1).to(device)
+                sum2_id = tokenizer(sum2)['input_ids'].squeeze(1).to(device)
+                        
+                label = label.to(device)
+                predicted_reward_1 = None
+                predicted_reward_2 = None
+
+                if label == 0:
+                    predicted_reward_1 = model(post_id, sum1_id)
+                    predicted_reward_2 = model(post_id, sum2_id)
+                else:
+                    predicted_reward_2 = model(post_id, sum1_id)
+                    predicted_reward_1 = model(post_id, sum2_id)
+        
+            
+                batch_loss = criterion(predicted_reward_1 - predicted_reward_2)
+                total_loss_val += batch_loss.item()
+
+                
+                # acc = (output.argmax(dim=1) == val_label).sum().item()
+                # total_acc_val += acc
+        
+        print(
+            f'Epochs: {epoch_num + 1} | Train Loss: {total_loss_train / len(train_data): .3f} \
+            | Train Accuracy: {total_acc_train / len(train_data): .3f} \
+            | Val Loss: {total_loss_val / len(val_data): .3f} \
+            | Val Accuracy: {total_acc_val / len(val_data): .3f}')
+                  
+EPOCHS = 5
+LR = 1e-6
+train(model, df_train, df_val, LR, EPOCHS)
 
 
 
