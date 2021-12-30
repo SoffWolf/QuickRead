6dd6147b173d76e1206d159b75f998af5dae0ef8
SoffWolf <tungnguyen21012000@gmail.com>
2021-12-30 17:38:43 +0200

    Test the first reward model

--- reward_model.py
+++ reward_model.py
@@ -5,9 +5,10 @@ import numpy as np
 class RewardModel(nn.Module):
     def __init__(self, supervised_baseline, d_model=1024, init_scales=0.0):
         super(RewardModel, self).__init__()
-        # self.init_scales = init_scales 
-        # self.d_model = d_model
-        self.supervised_baseline =  supervised_baseline
+
+        self.post_input = supervised_baseline
+        self.summary_input = supervised_baseline
+        self.final = nn.Sequential(nn.Linear(2*d_model, d_model))
 
         # Add a randomly initialized linear head that outputs a scalar value
         init_std = init_scales / np.sqrt(d_model + 1)  #.get(name, 1.0)
@@ -17,10 +18,10 @@ class RewardModel(nn.Module):
         self.head = head
 
     def forward(self, post_tokens, summary_tokens):
-        x = self.supervised_baseline(post)
-        y = self.NeuralNet(summary)
-        z = torch.stack(x, y)
-        z = self.n2(z)
+        x = self.post_input(post)
+        y = self.summary_input(summary)
+        z = torch.stack([x, y], 1)
+        z = self.final(z)
         # go through custom layer
         reward = self.head(z)
         return reward
