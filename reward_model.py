467d94b402a012b31b94f21e1035fb3298c2190d
SoffWolf <tungnguyen21012000@gmail.com>
2021-12-30 18:07:14 +0200

    Update Reward model and add some input in alt file

--- reward_model.py
+++ reward_model.py
@@ -1,14 +1,15 @@
+import torch
 import torch.nn as nn
 import numpy as np
 
 
 class RewardModel(nn.Module):
-    def __init__(self, supervised_baseline, d_model=1024, init_scales=0.0):
+    def __init__(self, supervised_baseline, d_model=64, init_scales=0.0):
         super(RewardModel, self).__init__()
 
         self.post_input = supervised_baseline
         self.summary_input = supervised_baseline
-        self.final = nn.Sequential(nn.Linear(2*d_model, d_model))
+        self.final = nn.Sequential(nn.Linear(d_model, d_model))
 
         # Add a randomly initialized linear head that outputs a scalar value
         init_std = init_scales / np.sqrt(d_model + 1)  #.get(name, 1.0)
@@ -18,10 +19,10 @@ class RewardModel(nn.Module):
         self.head = head
 
     def forward(self, post_tokens, summary_tokens):
-        x = self.post_input(post)
-        y = self.summary_input(summary)
-        z = torch.stack([x, y], 1)
-        z = self.final(z)
+        x = self.post_input.generate(input_ids=post_tokens)
+        y = self.summary_input.generate(input_ids=summary_tokens)
+        z = torch.concat((x, y), axis=1)
+        z = self.final(z.type(torch.FloatTensor))
         # go through custom layer
         reward = self.head(z)
         return reward
