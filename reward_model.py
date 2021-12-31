50a8965ae8c5da5f635d7db3b526afe060d0e1d1
Nguyen Tung <tungnguyen21012000@gmail.com>
2021-12-31 20:20:30 +0200

    Finish the training loop for reward model

--- reward_model.py
+++ reward_model.py
@@ -18,16 +18,11 @@ class RewardModel(nn.Module):
         self.head = head
 
     def forward(self, post_tokens, summary_tokens):
-        x = torch.cat((post_tokens, summary_tokens), dim=1) 
+        x = torch.concat((post_tokens, summary_tokens), axis=1) 
         x = self.supervised_baseline.generate(input_ids=x)
         x = F.pad(input=x, pad=(1, self.d_model - x.shape[1] - 1), mode='constant', value=0)
-        print("x.shape: ", x.shape)
-        print("post_tokens.shape: ", post_tokens.shape)
-        print("summary_tokens.shape: ", summary_tokens.shape)
         # go through custom layer
         reward = self.head(x.type(torch.FloatTensor))
-        print("reward: \n", reward)
-        print("reward.shape: ", reward.shape)
         return reward
 
 
