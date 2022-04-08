b1874ccdb93d117e616a17fc090afbd19df76160
Tung Nguyen <tung.nguyen@aalto.fi>
2022-04-08 19:37:08 +0300

    add _eval method to reward model

--- rewards/reward_model.py
+++ rewards/reward_model.py
@@ -35,6 +35,16 @@ def _response_indices(response_tokens):
     return torch.max(indices, torch.zeros([1], dtype=indices.dtype, device=response_tokens.device))
 
 
+def _zero_padding_tokens(response_tokens):
+    mask = response_tokens == PADDING_TOKEN
+    assert (
+        not (mask[:, :, 1:] < mask[:, :, :-1]).any().item()
+    ), f"Padding tokens not a suffix {to_numpy(response_tokens)}"
+    return mask, torch.masked_fill(response_tokens, mask, 0)
+
+
+
+
 class RewardModel(nn.Module):
     def __init__(self, supervised_baseline, d_model=1024, init_scales=1.0):
         super(RewardModel, self).__init__()
@@ -52,6 +62,35 @@ class RewardModel(nn.Module):
         self.device = device
         self.head = self.head.to(self.device)
         
+    def _eval(queries, responses):
+        """
+        Run a forward pass. Return all the head values, broadcasted within each replica. If an
+        eval_fn is passed, return its output across all replicas.
+
+        :return: A dict with structure:
+            eval_stats: structure from eval_fn
+            [head]: {
+                # disabled for now: query: [batch, query_len+1]
+                response: [batch, num_responses, sample_len+1]
+            }
+        """
+        queries = queries.to(self.device)
+        responses = responses.to(self.device)
+        mask, responses = _zero_padding_tokens(responses)
+        responses_per_query = responses.size(1)
+        # NOTE: could make this more efficient by sharing context work
+        tiled_queries = queries.unsqueeze(1).repeat(1, responses_per_query, 1)
+        run_tokens = torch.cat([tiled_queries, responses], dim=2).flatten(0, 1)
+
+        self.model.eval()
+        with torch.no_grad():
+            outputs = self.head(run_tokens)
+        outputs_mb = dict()
+            
+        reshaped = outputs.view(-1, responses_per_query, outputs.size()[1:])
+        d = _split_query_response_output_parts(reshaped, queries.size(1), mask)
+        return d
+
     def forward(self, post_tokens, summary_tokens):
         len_post = post_tokens.shape[-1]
         print(len_post)
@@ -68,14 +107,17 @@ class RewardModel(nn.Module):
         # go through custom layer
         x = outputs.encoder_last_hidden_state
         print("Shape of x: ", x.shape)
-        values = self.head(x.to(self.device))
-        print("Shape of values: ", values.shape)
-        values = values.squeeze(dim=-1)
-        print("Shape of value after squeeze: ", values.shape)
-        
-        response_values = values[:, len_post:] 
-        response_values = response_values.to(self.device)
-        print("Shape of response_values: ", response_values.shape)
+        # values = self.head(x.to(self.device))
+        # print("Shape of values: ", values.shape)
+        # values = values.squeeze(dim=-1)
+        # print("Shape of value after squeeze: ", values.shape)
+        # 
+        # response_values = values[:, len_post:] 
+        # response_values = response_values.to(self.device)
+        # print("Shape of response_values: ", response_values.shape)
+
+        #new
+        response_values = self._eval(post_tokens, summary_tokens)
         
         last_response_indices = _response_indices(summary_tokens)
         print("Shape of last_response_indices: ", last_response_indices.shape)
