98025a14af8f3fba0d8c955207a51e0c3774228f
SophieTruong <sophietruong1992@gmail.com>
2022-04-20 19:38:06 +0300

    Removed prints, change lr during 1 epoch, add test function

--- rewards/reward_train.py
+++ rewards/reward_train.py
@@ -105,7 +105,6 @@ supervised_baseline = PegasusModel.from_pretrained("QuickRead/pegasus-reddit-7e0
 model = RewardModel(supervised_baseline)
 keys_file = open("../PPO_training/hfAPI.txt")
 key = keys_file.readlines()[0].rstrip()
-#print(key)
 
 ### TO BE UNCOMMENT AFTER DEBUG
 #save_directory = "QuickRead/Reward_training_Pegasus_reddit"
@@ -114,26 +113,11 @@ key = keys_file.readlines()[0].rstrip()
 
 #Collate
 def collate(list_of_samples):
-    """Merges a list of samples to form a mini-batch.
-
-    Args:
-      list_of_samples is a list of tuples (src_seq, tgt_seq):
-          src_seq is of shape (src_seq_length)
-          tgt_seq is of shape (tgt_seq_length)
-
-    Returns:
-      src_seqs of shape (max_src_seq_length, batch_size): LongTensor of padded source sequences.
-      src_mask of shape (max_src_seq_length, batch_size): BoolTensor (tensor with boolean elements) indicating which
-          elements of the src_seqs tensor should be ignored in computations: True values in src_mask correspond
-          to padding values in src_seqs.
-      tgt_seqs of shape (max_tgt_seq_length+1, batch_size): LongTensor of padded target sequences.
-    """
-    # YOUR CODE HERE
+    """Merges a list of samples to form a mini-batch."""
     posts = []
     post_mask = []
     sum1s = []
     sum2s = []
-    print("into collate: ", list_of_samples[0])
     # paste data to src_seqs, tgt_seqs
     for i in list_of_samples:
         posts.append(torch.squeeze(i[0],0))
@@ -141,14 +125,10 @@ def collate(list_of_samples):
         sum2s.append(torch.squeeze(i[2],0))
         #print("done appending: ", i[0].shape, "\n sum1: ", i[1].shape, "\n sum2: ", i[2].shape)
     # zero-padding 
-    print("before pad: ", len(posts), len(sum1s), len(sum2s))
-    print("before pad 2: ", len(posts[0]), len(sum1s[0]), len(sum2s[0]))
     posts = pad_sequence(posts, True, padding_value=-1)
     sum1s = pad_sequence(sum1s, True, padding_value=-1)
     sum2s = pad_sequence(sum2s, True, padding_value=-1)
-    print("after pad: ", posts.shape, sum1s.shape, sum2s.shape)
     #print("Done padding: ", posts.shape, sum1s.shape, sum2s.shape)
-    # transfor src_seqs to src_mask
     for i in posts:
         row = []
         for j in i:
@@ -158,15 +138,6 @@ def collate(list_of_samples):
                 row.append(0)
         post_mask.append(row)
     post_mask = torch.BoolTensor(post_mask)                 # tranform to right dtype: torch.bool
-
-    #sos1_ = torch.zeros(1, sum1s.shape[1])             # create sos_token with right shape
-    #sos2_ = torch.zeros(1, sum2s.shape[1])             # create sos_token with right shape
-    #sum1s = torch.cat((sos1_, sum1s), dim = 0).long() # insert to tgt_seqs as 1st row
-    #sum2s = torch.cat((sos2_, sum2s), dim = 0).long() # insert to tgt_seqs as 1st row
-    #print("et o et: ", sum1s.shape, sum2s.shape)
-#     print(f'tgt_seqs:\n {tgt_seqs}')
-#     print(f'src_seqs:\n {src_seqs}')
-#     print(f'src_mask:\n {src_mask}')
     return posts, sum1s, sum2s
 
 
@@ -197,10 +168,7 @@ def train(model, train_data, val_data, learning_rate, epochs, bs):
         ret = torch.log(sigmoid_r)
         m = nn.LogSigmoid()
         ret = m(x) * - 1
-        print("ret: ", ret)
-        print("ret type: ", type(ret))
         ret = torch.mean(ret)
-        print("after mean: ", ret)
         #print("\n ret from criterion = ", ret)
         return ret
 
@@ -216,45 +184,27 @@ def train(model, train_data, val_data, learning_rate, epochs, bs):
 
     # WANDB 
     wandb.watch(model, log="all")
-    print("ENTERING FOR EPOCH LOOP") 
     for epoch_num in range(epochs):
         total_acc_train = 0
         total_loss_train = 0
         acc_per_100 = 0
         step = 0
-        print("BEFORE TRAIN LOOP")
+        model.train()
         for post, sum1, sum2 in tqdm(train_dataloader):
             # Input
-            print(post, "\n", sum1, "\n", sum2)
             post_id = post.to(device)
             sum1_id = sum1.to(device)
             sum2_id = sum2.to(device)
-            #print("TYPES: ", post_id.dtype, sum1_id.dtype, sum2_id.dtype)
-            #print("SHAPES: ", post_id.shape, sum1_id.shape, sum2_id.shape)
-            #post_id, sum1_id, sum2_id = post_id.to(device), sum1_id.to(device), sum2_id.to(device)
-            print("SHAPES of post_id, sum1_id, sum2_id: ", post_id.shape, sum1_id.shape, sum2_id.shape)
-            #try:
-                # Output rewards
             
-            print("GET after inputs, before MODEL call")
+            # Output rewards
             predicted_reward_1 = model(post_id, sum1_id)
             predicted_reward_2 = model(post_id, sum2_id)
-            print("predicted_reward_1: ", predicted_reward_1.shape,"\n", predicted_reward_1)
-            print("predicted_reward_2: ",predicted_reward_2.shape, "\n", predicted_reward_2
-)
-            #except Exception as e: 
-                #print("ERROR IN TRAIN LOOP (1)")
-                #print(e)
-                #print("SHAPES: ", post_id.shape, sum1_id.shape, sum2_id.shape)
-                #continue
             optimizer.zero_grad()
 
             # Loss and accuracy
             batch_loss = criterion(torch.sub(predicted_reward_1,predicted_reward_2))
-            print("batch_loss = ", batch_loss)
             total_loss_train += batch_loss
             step += 1
-            # print("train batch loss: ", batch_loss)
             
             # ACC increases when predicted_reward_1 is larger than predicted_reward_2 ??? 
             acc = (predicted_reward_1 > predicted_reward_2).sum().item()
@@ -268,7 +218,6 @@ def train(model, train_data, val_data, learning_rate, epochs, bs):
 
             if step % 100 == 0:
                 acc_per_100 = acc_per_100/(bs * 100)
-                #   print("train batch total_acc_train/step: ", acc_per_100)
                 # Logging
                 wandb.log({ "train/batch-loss": batch_loss,
                             "train/total-batch-loss": total_loss_train,
@@ -276,12 +225,35 @@ def train(model, train_data, val_data, learning_rate, epochs, bs):
                             "train/batch-total_acc_train-per-100-step": acc_per_100})
                 acc_per_100 = 0
 
-
+            if step % (100*1000) == 0:
+                print("Step where the learning rate is changed from 1e-6 to 9e-7: ", step)
+                print("Previous LR = ", optimizer.param_groups[0]['lr'])
+                optimizer.param_groups[0]['lr'] = 9e-7
+                print("LR after updated = ", optimizer.param_groups[0]['lr'],"\n-------------------------------\n")
+            
+            if step % (100*1200) == 0:
+                print("Step where the learning rate is changed from 9e-7 to 7e-7: ", step)
+                print("Previous LR = ", optimizer.param_groups[0]['lr'])
+                optimizer.param_groups[0]['lr'] = 7e-7
+                print("LR after updated = ", optimizer.param_groups[0]['lr'],"\n-------------------------------\n")
+            
+            if step % (100*1400) == 0:
+                print("Step where the learning rate is changed from 7e-7 to 5e-7: ", step)
+                print("Previous LR = ", optimizer.param_groups[0]['lr'])
+                optimizer.param_groups[0]['lr'] = 5e-7
+                print("LR after updated = ", optimizer.param_groups[0]['lr'],"\n-------------------------------\n")
+            
+            if step % (100*1500) == 0:
+                print("Step where the learning rate is changed from 5e-7 to 2e-7: ", step)
+                print("Previous LR = ", optimizer.param_groups[0]['lr'])
+                optimizer.param_groups[0]['lr'] = 2e-7
+                print("LR after updated = ", optimizer.param_groups[0]['lr'],"\n-------------------------------\n")
+                
         total_acc_val = 0
         total_loss_val = 0
         step = 0
         acc_per_100 = 0
-        print("BEFORE VAL LOOP")
+        model.eval()
         with torch.no_grad():
 
             for post, sum1, sum2 in tqdm(val_dataloader):
@@ -291,41 +263,20 @@ def train(model, train_data, val_data, learning_rate, epochs, bs):
                 sum1_id = sum1.to(device)
                 sum2_id = sum2.to(device)
 
-                #try:
-                    # Output rewards
+                #Output rewards
                 predicted_reward_1 = model(post_id, sum1_id)
                 predicted_reward_2 = model(post_id, sum2_id)
-                    #print("predicted_reward_1: ", predicted_reward_1)
-                    #print("predicted_reward_2: ",predicted_reward_2)
-                #except: 
-                    #print("ERROR IN TRAIN LOOP (2)")
-                    #print("SHAPES: ", post_id.shape, sum1_id.shape, sum2_id.shape)
-                    #continue
-
-                # # Output rewards
-                # predicted_reward_1 = model(post_id, sum1_id, device=device)
-                # predicted_reward_2 = model(post_id, sum2_id, device=device)
-                # print("predicted_reward_1: ", predicted_reward_1)
-                # print("predicted_reward_2: ",predicted_reward_2)
 
                 # Loss and accuracy
                 batch_loss = criterion(torch.sub(predicted_reward_1,predicted_reward_2))
                 total_loss_val+= batch_loss
                 step += 1
-                #print("eval batch loss: ", batch_loss)
 
                 acc = (predicted_reward_1 > predicted_reward_2).sum().item()                
                 total_acc_val += acc
                 acc_per_100 += acc
-                # # Logging
-                # wandb.log({ "val/batch-loss": batch_loss,
-                #             "val/total-batch-loss": total_loss_val,
-                #             "val/total-batch-acc": total_acc_val,
-                #             "val/batch-acc-per-step" :acc/step, 
-                #             "val/batch-total_acc_val-per-step": total_acc_val/step})
                 if step % 100 == 0:
                     acc_per_100 = acc_per_100/(bs * 100)
-                    # print("val batch total_acc_val/100 step: ", acc_per_100)
                     
                     # Logging
                     wandb.log({ "val/batch-loss": batch_loss,
@@ -333,10 +284,6 @@ def train(model, train_data, val_data, learning_rate, epochs, bs):
                                 "val/total-batch-acc": total_acc_val,
                                 "val/batch-total_acc-per-100-step": acc_per_100})
                     acc_per_100 = 0
-
-        print(
-            f'Epochs: {epoch_num + 1} | Train Loss: {total_loss_train / len(train_data): .3f} \
-            | Val Loss: {total_loss_val / len(val_data): .3f}\n')
         print(
               f'Epochs: {epoch_num + 1} \
               | Train Loss: {total_loss_train / len(train_data): .3f} \
@@ -350,30 +297,47 @@ def train(model, train_data, val_data, learning_rate, epochs, bs):
                    "val/Epoch-val-loss": total_loss_val / len(val_data),
                    "val/Val-acc": total_acc_val / len(val_data) })
 
-        # Print model's state_dict
-        #print("Model's state_dict:")
-        #for param_tensor in model.state_dict():
-            #print(param_tensor, "\t", model.state_dict()[param_tensor].size())
-
-        # Print optimizer's state_dict
-        #print("Optimizer's state_dict:")
-        #for var_name in optimizer.state_dict():
-            #print(var_name, "\t", optimizer.state_dict()[var_name])
-        
     # Save model
     checkpoint = {'state_dict': model.state_dict(),'optimizer' :optimizer.state_dict()}
-    # torch.save(model.state_dict(), PATH)
     torch.save(checkpoint, os.path.join("./reward_model_wandb_7e5_bs_1_idx", 'epoch-{}.pth'.format(epoch_num+1)))
 
-    # torch.save(model, os.path.join("./reward_model_weight_5ep", 'epoch-{}.pth'.format(epoch_num+1)))
     model.push_to_hub("QuickRead/Reward_training_Pegasus_reddit")
     tokenizer.push_to_hub("QuickRead/Reward_training_Pegasus_reddit")
     
+    return model
+
+def test(model, df_test):
+    test_dataloader = torch.utils.data.DataLoader(df_test, collate_fn=collate)
+    total_acc_test = 0
+    step = 0
+    acc_per_100 = 0
+    model.eval()
+    with torch.no_grad():
+        for post, sum1, sum2 in tqdm(test_dataloader):
+
+            # Input
+            post_id = post.to(device)
+            sum1_id = sum1.to(device)
+            sum2_id = sum2.to(device)
+
+            #Output rewards
+            predicted_reward_1 = model(post_id, sum1_id)
+            predicted_reward_2 = model(post_id, sum2_id)
+
+            acc = (predicted_reward_1 > predicted_reward_2).sum().item()                
+            total_acc_test += acc
+            acc_per_100 += acc
+            if step % 100 == 0:
+                acc_per_100 = acc_per_100/(100)
+            acc_per_100 = 0
+    print(
+            f'Test accuracy: {total_acc_test / len(df_test): .3f}\n')
 
 EPOCHS = 1
 LR = 1e-6
 BATCH_SIZE = 1
-train(model, df_train, df_val, LR, EPOCHS, BATCH_SIZE)
+trained_model = train(model, df_train, df_val, LR, EPOCHS, BATCH_SIZE)
+test(trained_model, df_test)
 
 
 
