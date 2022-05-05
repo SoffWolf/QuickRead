e5a6359d980d2e79926acf2d63269bf82f66381a
SophieTruong <sophietruong1992@gmail.com>
2022-05-05 15:18:44 +0300

    Run train to print reward model

--- PPO_training/training.py
+++ PPO_training/training.py
@@ -107,86 +107,6 @@ else:
     ## Now the epoch is still gonna be retrained
 
 n_except = 0
-<<<<<<< HEAD
-for epoch in tqdm(range(int(np.ceil(len(train_texts) / config["batch_size"])))):
-#for epoch in tqdm(range(int(np.ceil(len(train_texts) / config["batch_size"])))):
-    torch.cuda.empty_cache()
-    logs = dict()
-    timing = dict()
-    t0 = time.time()
-
-    query_batch = df.sample(config["batch_size"])
-    query_tensors = []  # get query tensor for PPO training
-    response_tensors = []
-    rewards = []
-    
-    for i in range(int(config["batch_size"] / fbs)):
-        # try:
-            query = query_batch[i*fbs:(i+1)*fbs]
-            query = map(lambda x: x[0], query.values.tolist())
-            query = list(query)
-            query = tokenizer(query, padding=True, truncation=True, return_tensors='pt').input_ids
-            query = query.to(device)
-            # print("QUERY (", i, ") = ",query.shape)
-            response = policy.generate(query)
-            response = response.to(device)
-            # print("RESPONSE (", i, ") = ", response.shape)
-
-            reward_model.eval()
-            with torch.no_grad():
-                reward = reward_model(query, response).detach()
-            reward = reward.to(device)
-
-            query_tensors = query_tensors + list(torch.split(query,1))
-
-            response_tensors = response_tensors + list(torch.split(response,1))
-
-            rewards.append(reward)
-            
-            # print the generate result every 1000 epochs
-            if (epoch+1) % 1000 == 0:
-                print(response)
-
-        # except Exception as e1:
-        #     print(e1)
-        #     n_except =  n_except + 1
-        #     print("Number of EXCEPTS =", n_except)
-    for k in range(len(query_tensors)):
-        query_tensors[k] = query_tensors[k].squeeze(0)
-        response_tensors[k] = response_tensors[k].squeeze(0)
-
-    query_tensors = torch.nn.utils.rnn.pad_sequence(query_tensors)
-    response_tensors = torch.nn.utils.rnn.pad_sequence(response_tensors)
-    query_tensors = query_tensors.unsqueeze(dim=0).to(device)
-    response_tensors = response_tensors.unsqueeze(dim=0).to(device)
-    # print("Rewards before torch.cat: ", rewards)
-    rewards = torch.cat(rewards).to(device)
-    # print("Rewards after torch.cat: ", rewards)
-    query_tensors = query_tensors.view(query_tensors.shape[2], query_tensors.shape[1])
-    response_tensors = response_tensors.view(response_tensors.shape[2], response_tensors.shape[1])
-
-    #### Run PPO training 
-    stats = ppo_trainer.step(query_tensors, response_tensors, rewards)
-
-    #### Log everything
-    timing['time/epoch'] = time.time()-t0
-    logs.update(timing)
-    logs.update(stats)
-    logs['env/reward_mean'] = torch.mean(rewards).cpu().numpy()
-    logs['env/reward_std'] = torch.std(rewards).cpu().numpy()
-    logs['env/reward_dist'] = rewards.cpu().numpy()
-    wandb.log(logs)
-
-    if (epoch+1) % 2000 == 0:
-        # print("EPOCH: ", epoch)
-        # HF push_to_hub:
-        policy.push_to_hub("SophieTr/"+RUN_NAME)
-        tokenizer.push_to_hub("SophieTr/"+RUN_NAME)
-        # Save checkpoint (TOBE DONE)
-        checkpoint = {'state_dict': policy.state_dict(), 'epoch': epoch,}
-        torch.save( checkpoint, CHECKPOINT_PATH )
-        wandb.save(CHECKPOINT_PATH)
-=======
 for epoch in range(1):
     sample = df.sample(len(df))   #config["batch_size"])
 #for epoch in tqdm(range(int(np.ceil(len(train_texts) / config["batch_size"]))))::
@@ -203,75 +123,74 @@ for epoch in range(1):
         
         for i in range(int(config["batch_size"] / fbs)):
             # try:
-                query = query_batch[i*fbs:(i+1)*fbs]
-                query = map(lambda x: x[0], query.values.tolist())
-                query = list(query)
-                query = tokenizer(query, padding=True, truncation=True, return_tensors='pt').input_ids
-                query = query.to(device)
-                # print("QUERY (", i, ") = ",query.shape)
-                response = policy.generate(query)
-                response = response.to(device)
-                # print("RESPONSE (", i, ") = ", response.shape)
-                print(reward_model)
-                # print(summary(reward_model))
-                print("query_tensors: ", query)
-                print("response_tensors: ", response)
-                reward_model.eval()
-                with torch.no_grad():
-                    reward = reward_model(query, response).detach()
-                break
-                reward = reward.to(device)
+            query = query_batch[i*fbs:(i+1)*fbs]
+            query = map(lambda x: x[0], query.values.tolist())
+            query = list(query)
+            query = tokenizer(query, padding=True, truncation=True, return_tensors='pt').input_ids
+            query = query.to(device)
+            # print("QUERY (", i, ") = ",query.shape)
+            response = policy.generate(query)
+            response = response.to(device)
+            # print("RESPONSE (", i, ") = ", response.shape)
+            print("REWARD MODEL:\n ", reward_model)
+            # print(summary(reward_model))
+            print("\tquery_tensors: \n\t", query)
+            print("\tresponse_tensors: \n\t", response)
+            reward_model.eval()
+            with torch.no_grad():
+                reward = reward_model(query, response).detach()
+        break
+        #     reward = reward.to(device)
 
-                query_tensors = query_tensors + list(torch.split(query,1))
+        #     query_tensors = query_tensors + list(torch.split(query,1))
 
-                response_tensors = response_tensors + list(torch.split(response,1))
+        #     response_tensors = response_tensors + list(torch.split(response,1))
 
-                rewards.append(reward)
+        #     rewards.append(reward)
                 
-                # print the generate result every 1000 epochs
-                if (epoch+1) % 1000 == 0:
-                    print(response)
+        #         # print the generate result every 1000 epochs
+        #     if (k+1) % 1000 == 0:
+        #         print(response)
 
-            # except Exception as e1:
-            #     print(e1)
-            #     n_except =  n_except + 1
-            #     print("Number of EXCEPTS =", n_except)
-        for k in range(len(query_tensors)):
-            query_tensors[k] = query_tensors[k].squeeze(0)
-            response_tensors[k] = response_tensors[k].squeeze(0)
+        #     # except Exception as e1:
+        #     #     print(e1)
+        #     #     n_except =  n_except + 1
+        #     #     print("Number of EXCEPTS =", n_except)
+        # for k in range(len(query_tensors)):
+        #     query_tensors[k] = query_tensors[k].squeeze(0)
+        #     response_tensors[k] = response_tensors[k].squeeze(0)
 
-        query_tensors = torch.nn.utils.rnn.pad_sequence(query_tensors)
-        response_tensors = torch.nn.utils.rnn.pad_sequence(response_tensors)
-        query_tensors = query_tensors.unsqueeze(dim=0).to(device)
-        response_tensors = response_tensors.unsqueeze(dim=0).to(device)
-        # print("Rewards before torch.cat: ", rewards)
-        rewards = torch.cat(rewards).to(device)
-        # print("Rewards after torch.cat: ", rewards)
-        query_tensors = query_tensors.view(query_tensors.shape[2], query_tensors.shape[1])
-        response_tensors = response_tensors.view(response_tensors.shape[2], response_tensors.shape[1])
+        # query_tensors = torch.nn.utils.rnn.pad_sequence(query_tensors)
+        # response_tensors = torch.nn.utils.rnn.pad_sequence(response_tensors)
+        # query_tensors = query_tensors.unsqueeze(dim=0).to(device)
+        # response_tensors = response_tensors.unsqueeze(dim=0).to(device)
+        # # print("Rewards before torch.cat: ", rewards)
+        # rewards = torch.cat(rewards).to(device)
+        # # print("Rewards after torch.cat: ", rewards)
+        # query_tensors = query_tensors.view(query_tensors.shape[2], query_tensors.shape[1])
+        # response_tensors = response_tensors.view(response_tensors.shape[2], response_tensors.shape[1])
 
-        #### Run PPO training 
-        stats = ppo_trainer.step(query_tensors, response_tensors, rewards)
+        # #### Run PPO training 
+        # stats = ppo_trainer.step(query_tensors, response_tensors, rewards)
 
-        #### Log everything
-        timing['time/epoch'] = time.time()-t0
-        logs.update(timing)
-        logs.update(stats)
-        logs['env/reward_mean'] = torch.mean(rewards).cpu().numpy()
-        logs['env/reward_std'] = torch.std(rewards).cpu().numpy()
-        logs['env/reward_dist'] = rewards.cpu().numpy()
-        wandb.log(logs)
+        # #### Log everything
+        # timing['time/epoch'] = time.time()-t0
+        # logs.update(timing)
+        # logs.update(stats)
+        # logs['env/reward_mean'] = torch.mean(rewards).cpu().numpy()
+        # logs['env/reward_std'] = torch.std(rewards).cpu().numpy()
+        # logs['env/reward_dist'] = rewards.cpu().numpy()
+        # wandb.log(logs)
 
-        if (epoch+1) % 2000 == 0:
-            # print("EPOCH: ", epoch)
-            # HF push_to_hub:
-            policy.push_to_hub("SophieTr/"+RUN_NAME)
-            tokenizer.push_to_hub("SophieTr/"+RUN_NAME)
-            # Save checkpoint (TOBE DONE)
-            checkpoint = {'state_dict': policy.state_dict(), 'epoch': epoch,}
-            torch.save( checkpoint, CHECKPOINT_PATH )
-            wandb.save(CHECKPOINT_PATH)
->>>>>>> 9e400db093304228b3bdcab112b935c2716c4609
+        # if (epoch+1) % 2000 == 0:
+        #     # print("EPOCH: ", epoch)
+        #     # HF push_to_hub:
+        #     policy.push_to_hub("SophieTr/"+RUN_NAME)
+        #     tokenizer.push_to_hub("SophieTr/"+RUN_NAME)
+        #     # Save checkpoint (TOBE DONE)
+        #     checkpoint = {'state_dict': policy.state_dict(), 'epoch': epoch,}
+        #     torch.save( checkpoint, CHECKPOINT_PATH )
+        #     wandb.save(CHECKPOINT_PATH)
 
         
 # HF push_to_hub:
