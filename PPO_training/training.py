b396a662ad64e6453f2253f124b30978558ab8b3
SophieTruong <sophietruong1992@gmail.com>
2022-05-06 15:53:33 +0300

    fixed v2 indentation err

--- PPO_training/training.py
+++ PPO_training/training.py
@@ -109,7 +109,7 @@ else:
 n_except = 0
 error_lst = []
 for epoch in range(1):
-    sample = df # shuffle(df)
+    sample = shuffle(df)
     if len(sample) != df.shape[0]:
         break
     #print(len(sample[0]), df.shape[0][0])
@@ -147,8 +147,9 @@ for epoch in range(1):
                 reward = reward.to(device)
             except Exception as e:
                 print(f'Error is: {e}\nError args: {e.args}\nError type: {type(e)}')
-		print(f'SHAPE OF ERROR QUERY: {query.shape}\n SHAPE OF ERROR RESPONSE: {response.shape}\n')
-                error_lst.append(k*8 + i) # [query, response])
+                print(f'SHAPE OF ERROR QUERY: {query.shape}\n SHAPE OF ERROR RESPONSE: {response.shape}\n')
+                # error_lst.append(k*8 + i) # [query, response])
+                error_lst.append([query, response])
 #             query_tensors = query_tensors + list(torch.split(query,1))
 # 
 #             response_tensors = response_tensors + list(torch.split(response,1))
@@ -208,14 +209,14 @@ for epoch in range(1):
 # checkpoint = {'state_dict': policy.state_dict()}
 # #torch.save(checkpoint, os.path.join("./result/test.pth"))
 # torch.save(checkpoint, os.path.join(PATH, 'epoch-{}.pth'.format(epoch+1)))
-# with open('err_query_response.txt', 'w') as f:
-#     i = 0
-#     for (q, r) in error_lst:
-#         f.write("Error " + i)
-# 	f.write(q)
-#         f.write('\n')
-# 	f.write(r)
-# 	f.write('--.--'*100)
-# 	f.write('\n')
-# 	i+=1
-print("Error lst: ", error_lst)
+with open('err_query_response.txt', 'w') as f:
+    i = 0
+    for (q, r) in error_lst:
+        f.write("Error " + i)
+        f.write(q)
+        f.write('\n')
+        f.write(r)
+        f.write('--.--'*100)
+        f.write('\n')
+        i+=1
+# print("Error lst: ", error_lst)
