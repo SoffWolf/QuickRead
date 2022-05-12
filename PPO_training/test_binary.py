dd7bd36ab34b14b6d5241131a8f82458250cd9b1
SophieTruong <sophietruong1992@gmail.com>
2022-05-12 11:56:43 +0300

    fixing bug

--- PPO_training/test_binary.py
+++ PPO_training/test_binary.py
@@ -21,7 +21,7 @@ tokenizer = PegasusTokenizer.from_pretrained("QuickRead/pegasus-reddit-7e05", ca
 
 # Policy
 policy = PegasusWithValueHead(supervised_baseline)
-policy.load_state_dict(torch.load(os.path.join(CHECKPOINT_PATH)), strict=False, map_location=torch.device('cpu'))
+policy.load_state_dict(torch.load(os.path.join(CHECKPOINT_PATH), map_location=torch.device('cpu')), strict=False)
 
 # Data
 # dataset = load_from_disk("../../../QuickRead/reddit_clean")
