1c5d8d4e5e0047e6c01d73c396d5987ffe45524f
SophieTruong <sophietruong1992@gmail.com>
2022-08-13 14:55:50 +0300

    fixed some reference error

--- reddit_preprocessing.py
+++ reddit_preprocessing.py
@@ -143,7 +143,7 @@ def chat_words_map_dict_factory():
     chat_words_map_dict = {}
     chat_words_list = []
     for line in chat_words_str.split("\n"):
-        if line != "":
+        if line[0] != "":
             print(line.split("="))
             cw = line.split("=")[0]
             cw_expanded = line.split("=")[1]
